82338c373332f306bb854e14338a7d13c92c26c3
Zhiwen Liu <90580132+real-rookie@users.noreply.github.com>
2023-11-22 13:39:52 -0700

    cpu_core done, but not tested

--- part_3/CPU_files/cpu_core.vhd
+++ part_3/CPU_files/cpu_core.vhd
@@ -20,34 +20,79 @@ USE IEEE.std_logic_1164.ALL;
 ENTITY cpu_core IS
 ---------------------------------------------------------------------------
 -- finish the port declaration with the appropriate types
-    PORT( clock         : IN 
-        ; reset         : IN 
-        ; enter         : IN 
-        ; user_input    : IN 
-        ; CPU_output    : OUT
-        ; PC_output     : OUT
-        ; OPCODE_output : OUT
-        ; done          : OUT
+    PORT( clock         : IN STD_LOGIC
+        ; reset         : IN STD_LOGIC
+        ; enter         : IN STD_LOGIC
+        ; user_input    : IN STD_LOGIC_VECTOR(7 DOWNTO 0)
+        ; CPU_output    : OUT STD_LOGIC_VECTOR(7 DOWNTO 0)
+        ; PC_output     : OUT STD_LOGIC_VECTOR(7 DOWNTO 0)
+        ; OPCODE_output : OUT STD_LOGIC_VECTOR(4 DOWNTO 0)
+        ; done          : OUT STD_LOGIC
         );
 ---------------------------------------------------------------------------
 END cpu_core;
 
 ARCHITECTURE Structural OF cpu_core IS
 ---------------------------------------------------------------------------
--- declare any necessary signals if any
+    signal clock_div        : STD_LOGIC;
+    signal mux_sel          : STD_LOGIC_VECTOR(1 downto 0);
+    signal immediate_data   : STD_LOGIC_VECTOR(7 downto 0);
+    signal acc_write        : STD_LOGIC;
+    signal acc_negation     : STD_LOGIC;
+    signal rf_address       : STD_LOGIC_VECTOR(2 downto 0);
+    signal rf_write         : STD_LOGIC;
+    signal alu_sel          : STD_LOGIC_VECTOR(2 downto 0);
+    signal output_enable    : STD_LOGIC;
+    signal zero_flag        : STD_LOGIC;
+    signal positive_flag    : STD_LOGIC;
+    signal bits_rotate      : STD_LOGIC_VECTOR(1 downto 0);
 ---------------------------------------------------------------------------
 BEGIN
 
     -- CPU clock divider, used to slow the processing of instructions
     -- allowing for manual testing. for simulation use freq_out <= 62_500_00
     core_div : ENTITY WORK.clock_divider(Behavioral)
-        GENERIC MAP (freq_out => 4)
+        GENERIC MAP (freq_out => 62_500_00)
         PORT MAP( clock => clock
                 , clock_div => clock_div
                 );
 
-    controller : -- instantiate controller entity
-
-    datapath : -- instantiate datapath entity
+    controller : ENTITY WORK.controller(Behavioral)-- instantiate controller entity
+        port map(
+            clock               => clock_div,
+            reset               => reset,
+            enter               => enter,
+            mux_sel             => mux_sel,
+            immediate_data      => immediate_data,
+            acc_write           => acc_write,
+            acc_negation        => acc_negation,
+            rf_address          => rf_address,
+            rf_write            => rf_write,
+            alu_sel             => alu_sel,
+            output_enable       => output_enable,
+            zero_flag           => zero_flag,
+            positive_flag       => positive_flag,
+            PC_out              => PC_output,
+            OPCODE_output       => OPCODE_output,
+            bits_rotate         => bits_rotate,
+            done                => done
+        );
+    datapath : ENTITY WORK.datapath(Structural)
+        port map(
+            clock => clock_div,
+            reset => reset,
+            mux_sel => mux_sel,
+            immediate_data => immediate_data,
+            user_input => user_input,
+            acc_write => acc_write,
+            rf_address => rf_address,
+            rf_write => rf_write,
+            alu_sel => alu_sel,
+            bits_rotate => bits_rotate,
+            output_enable => output_enable,
+            zero_flag => zero_flag,
+            positive_flag => positive_flag,
+            datapath_out => CPU_output
+        );
 
 END Structural;
