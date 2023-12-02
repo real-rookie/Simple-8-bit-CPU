262b522649c2db25b5bc4aca1ea3c23af5c2a065
Zhiwen Liu <90580132+real-rookie@users.noreply.github.com>
2023-12-02 11:07:20 -0700

    update

--- part_3/CPU_files/cpu_core_tb.vhd
+++ part_3/CPU_files/cpu_core_tb.vhd
@@ -66,8 +66,7 @@ BEGIN
         
         --*********************************
         -- provide the required input stimulus here for the design under test
-        --user_input <= "01010101"; -- loops between 2-11
-        user_input <= "00000001"; -- enters inst 12
+        user_input <= "00000010";
         enter <= '1';
         -----------------------------------
 
